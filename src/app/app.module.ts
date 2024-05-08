a2c55ec29000fb795ad2c2246bfb870236d427e8
VIS-WAS <vishwas.ms8830@gmail.com>
2024-05-08 20:45:51 +0530

    application is created to receive data from user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
+import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, DashboardComponent, CreateTaskComponent, TaskDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
