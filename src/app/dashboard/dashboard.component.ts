a4c16e584cddb1e697b24a998492f57fbf50dc82
VIS-WAS <vishwas.ms8830@gmail.com>
2024-05-09 20:48:49 +0530

    fetching data with GET request and display in UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
+
+  allTasks: Task[] = [];
+
+  ngOnInit() {
+    this.fetchAllTasks();
+  }
 
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
@@ -27,7 +34,34 @@ export class DashboardComponent {
         { headers: headers }
       )
       .subscribe((response) => {
-        console.log(response);
+        // this.fetchAllTasks();   //------to fetch directly after form is submitted. This is move to below fucntion
+      });
+  }
+  fetchAllTask() {
+    this.fetchAllTasks();
+  }
+  private fetchAllTasks() {
+    this.http
+      .get<{ [key: string]: Task }>(
+        'https://angularhttpclient-1cb37-default-rtdb.firebaseio.com/tasks.json'
+      )
+      .pipe(
+        map((response) => {
+          //TRANSFORM DATA
+
+          let tasks = [];
+          for (let key in response) {
+            if (response.hasOwnProperty(key)) {
+              // to make sure only actual property is going to add
+              tasks.push({ ...response[key], id: key });
+            }
+          }
+
+          return tasks;
+        })
+      )
+      .subscribe((tasks) => {
+        this.allTasks = tasks;
       });
   }
 }
