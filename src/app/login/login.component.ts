99e7b678474ec2bda8a8d7c21c391753ddd63235
VIS-WAS <vishwas.ms8830@gmail.com>
2024-05-26 20:40:54 +0530

    form is converted as TDF

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginFormService } from '../Services/login-form.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,14 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   isLoginMode: boolean = true;
 
+  loginService: LoginFormService = inject(LoginFormService);
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+  formSubmitted(form: NgForm) {
+    console.log(form.value);
+    // this.loginService.sendDetails(form.value);
+    form.reset();
   }
 }
