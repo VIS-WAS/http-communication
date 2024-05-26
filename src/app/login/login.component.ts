370a9b21df2a97bc2bd7476bda5d4f3de47ab463
VIS-WAS <vishwas.ms8830@gmail.com>
2024-05-26 19:40:18 +0530

    few modification to login form - login-signup toggle

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  isLoginMode: boolean = true;
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
