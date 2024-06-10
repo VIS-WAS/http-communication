7cb1013dae025fd3d1222b070131d8f1b4164f79
VIS-WAS <vishwas.ms8830@gmail.com>
2024-06-10 18:05:43 +0530

    adding canActivate Route Gourd

--- src/app/RouteGaurds/authGaurd.ts
+++ src/app/RouteGaurds/authGaurd.ts
@@ -1,17 +1,34 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { AuthService } from '../Services/auth.service';
-import { map } from 'rxjs';
+import { Observable, map, take } from 'rxjs';
 
 export const canActivate = (
   router: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
-) => {
+):
+  | boolean
+  | UrlTree
+  | Promise<boolean | UrlTree>
+  | Observable<boolean | UrlTree> => {
   const authService = inject(AuthService);
+  const route = inject(Router);
 
   return authService.user.pipe(
+    take(1),
     map((user) => {
-      return user ? true : false;
+      const loggedIn = user ? true : false;
+
+      if (loggedIn) {
+        return true;
+      } else {
+        return route.createUrlTree(['/login']);
+      }
     })
   );
 };
